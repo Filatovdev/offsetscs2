ae1db023c7711763158625d7a029808685247f33
Filatovdev <stretovich1006@gmail.com>
2025-10-02 10:26:14 +0300

    update offsets

--- output/interfaces.cs
+++ output/interfaces.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-02 07:09:52.155000700 UTC
+// 2025-10-02 07:26:13.946057700 UTC
 
 namespace CS2Dumper.Interfaces {
     // Module: animationsystem.dll
