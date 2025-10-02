9192eadd294b56f04e571eb56e55c07ba13b4a60
Filatovdev <stretovich1006@gmail.com>
2025-10-02 10:28:43 +0300

    update offsets

--- output/pulse_system_dll.rs
+++ output/pulse_system_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-02 07:26:13.946057700 UTC
+// 2025-10-02 07:28:42.675868500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- output/server_dll.cs
+++ output/server_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-02 07:26:13.946057700 UTC
+// 2025-10-02 07:28:42.675868500 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: server.dll
