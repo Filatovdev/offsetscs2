4c456065ce4c159c2d15a8344aa2e87521351518
Filatovdev <stretovich1006@gmail.com>
2025-10-04 21:00:05 +0300

    update offsets

--- output/interfaces.rs
+++ output/interfaces.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-04 17:58:21.494184300 UTC
+// 2025-10-04 18:00:04.506839700 UTC
 
 #![allow(non_upper_case_globals, unused)]
 
--- output/materialsystem2_dll.cs
+++ output/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-04 17:58:21.494184300 UTC
+// 2025-10-04 18:00:04.506839700 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
